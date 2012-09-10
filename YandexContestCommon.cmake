6842e27838db46c835d3a1dec2d62a46b6523a66
Aleksey Filippov <sarum9in@gmail.com>
2012-09-10 13:33:30 +0400

    New cmake function: add_binary_targets().

--- YandexContestCommon.cmake
+++ YandexContestCommon.cmake
@@ -48,6 +48,27 @@ if("${isSystemDir}" STREQUAL "-1")
     set(CMAKE_INSTALL_RPATH "${CMAKE_INSTALL_PREFIX}/lib")
 endif()
 
+# binary targets
+function(add_binary_targets bin_targets_)
+    set(bin_targets)
+
+    foreach(bin ${ARGN})
+        set(trgt ${PROJECT_NAME}_${bin})
+        list(APPEND bin_targets ${trgt})
+        set(binprefix src/bin/${bin})
+        if(EXISTS ${CMAKE_SOURCE_DIR}/${binprefix}.cpp)
+            add_executable(${trgt} ${binprefix}.cpp)
+        elseif(EXISTS ${CMAKE_SOURCE_DIR}/${binprefix}.c)
+            add_executable(${trgt} ${binprefix}.c)
+        else()
+            message(SEND_ERROR "Source does not exist for target ${trgt}")
+        endif()
+        target_link_libraries(${trgt} ${PROJECT_NAME} ${libraries})
+    endforeach()
+
+    set(${bin_targets_} ${bin_targets} PARENT_SCOPE)
+endfunction()
+
 # testing
 macro(yandex_contest_tests)
     if(${ENABLE_TESTS})

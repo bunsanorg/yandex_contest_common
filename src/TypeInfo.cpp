de1d1aba67fb78dcefbc13475829d4b6fb6d2f64
Aleksey Filippov <sarum9in@gmail.com>
2012-09-20 15:10:43 +0400

    typeinfo::demangle() better implementation.

--- src/TypeInfo.cpp
+++ src/TypeInfo.cpp
@@ -1,12 +1,29 @@
 #include "yandex/contest/TypeInfo.hpp"
 
-#include <boost/units/detail/utility.hpp>
+#include <boost/scope_exit.hpp>
+
+#include <cxxabi.h>
 
 namespace yandex{namespace contest{namespace typeinfo
 {
     std::string demangle(const char *const name)
     {
-        return boost::units::detail::demangle(name);
+        char *realname = nullptr;
+        BOOST_SCOPE_EXIT_ALL(realname)
+        {
+            if (realname)
+                std::free(realname);
+        };
+
+        std::size_t len;
+        int stat;
+
+        realname = abi::__cxa_demangle(name, nullptr, &len, &stat);
+
+        if (realname)
+            return std::string(realname);
+        else
+            return name;
     }
 
     std::string demangle(const std::string &name)

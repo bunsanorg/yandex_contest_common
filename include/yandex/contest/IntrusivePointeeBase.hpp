9c14a11b381fd82bf1d15ebbd073bd862e399f3d
Aleksey Filippov <sarum9in@gmail.com>
2013-03-25 00:18:10 +0400

    IntrusivePointeeBase implementation.

---
+++ include/yandex/contest/IntrusivePointeeBase.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <boost/noncopyable.hpp>
+
+#include <atomic>
+
+#include <cstddef>
+
+namespace yandex{namespace contest
+{
+    class IntrusivePointeeBase: private boost::noncopyable
+    {
+    public:
+        IntrusivePointeeBase(): refCount_(0) {}
+        virtual ~IntrusivePointeeBase() {}
+
+    private:
+        friend void intrusive_ptr_add_ref(IntrusivePointeeBase *) noexcept;
+        friend void intrusive_ptr_release(IntrusivePointeeBase *) noexcept;
+        std::atomic<std::size_t> refCount_;
+    };
+}}

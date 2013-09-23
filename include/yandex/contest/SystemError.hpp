94ef47d43a557d532319a2c34b942cea62d75f28
Aleksey Filippov <sarum9in@gmail.com>
2013-09-23 16:15:39 +0400

    SystemError refactoring.

--- include/yandex/contest/SystemError.hpp
+++ include/yandex/contest/SystemError.hpp
@@ -2,14 +2,13 @@
 
 #include <yandex/contest/Error.hpp>
 
-#include <bunsan/forward_constructor.hpp>
 #include <bunsan/system_error.hpp>
 
 namespace yandex{namespace contest
 {
-    struct SystemError: virtual Error, virtual bunsan::system_error
+    struct SystemError: bunsan::system_error, virtual Error
     {
-        BUNSAN_INHERIT_EXPLICIT_CONSTRUCTOR(SystemError, bunsan::system_error)
+        using bunsan::system_error::system_error;
 
         typedef error_code errorCode;
         typedef what_message whatMessage;

0dfe21ecb3dc92b204eacd4a8648b21f8bd40d41
Aleksey Filippov <sarum9in@gmail.com>
2015-03-14 17:02:34 +0100

    Removed APPEND logging interface.

--- include/yandex/contest/StreamLog.hpp
+++ include/yandex/contest/StreamLog.hpp
@@ -11,12 +11,6 @@
  * Useful defines for logging.
  * These defines determine logging position.
  *
- * \section append_iface_sec append() interface
- * APPEND_{Level} defines accept single argument: std::string (or implicitly convertible to) message.
- * \code{.cpp}
- * APPEND_INFO("I am info message!");
- * \endcode
- *
  * \section streaming_iface_sec streaming interface
  * STREAM_{Level} defines accept no arguments.
  * Using operator<< user can output anything supported by std::ostream.
@@ -34,17 +28,6 @@
 // common
 
 #define LOG_CURRENT_POSITION (std::string(BOOST_CURRENT_FUNCTION) + std::string(":") + boost::lexical_cast<std::string>(__LINE__))
-
-// Using append() interface.
-
-#define APPEND_LOG(LEVEL, MESSAGE) ::yandex::contest::Log::instance()->append(LEVEL, LOG_CURRENT_POSITION, MESSAGE)
-#define APPEND_LOG_BYNAME(LEVEL, MESSAGE) APPEND_LOG(::yandex::contest::contest::invoker::Log::Level::LEVEL, MESSAGE)
-
-#define APPEND_TRACE(MESSAGE) APPEND_LOG_LOG_BYNAME(TRACE, MESSAGE)
-#define APPEND_DEBUG(MESSAGE) APPEND_LOG_LOG_BYNAME(DEBUG, MESSAGE)
-#define APPEND_INFO(MESSAGE) APPEND_LOG_LOG_BYNAME(INFO, MESSAGE)
-#define APPEND_WARNING(MESSAGE) APPEND_LOG_LOG_BYNAME(WARNING, MESSAGE)
-#define APPEND_ERROR(MESSAGE) APPEND_LOG_LOG_BYNAME(ERROR, MESSAGE)
 
 // Using streaming interface.
 
